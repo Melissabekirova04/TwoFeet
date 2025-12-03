64fc091f583e4b5614bd8da58451d1218692f4d5
Melissabekirova04 <229076304+Melissabekirova04@users.noreply.github.com>
2025-12-03 12:52:08 +0100

    lavet lidt

---
+++ src/HelpCategory.java
@@ -0,0 +1,7 @@
+public enum HelpCategory {
+    LAUNDRY,       // Tøjvask
+    ELECTRONICS,   // Køleskab/fryser, strøm osv.
+    MOVE_IN,       // Inden indflytning
+    STARTERPACK,   // Hvad man skal have derhjemme
+    CLEANING       // Rengøringsrutine
+}
